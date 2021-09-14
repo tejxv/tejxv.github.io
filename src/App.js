433ff65e7546bd94b5d14a079e9447e04ac81bc7
tejas <54097365+tejxv@users.noreply.github.com>
2021-09-15 00:10:21 +0530

    nice

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Redir from "./Redir"
+import Home from "./Home"
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/m" component={Redir} />
+        <Route exact path="/" component={Home} />
       </Switch>
     </Router>
   );
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Redir() {
+    return (
+        <div>
+            <script>
+            setTimeout(function() {
+    window.location.replace('https://www.instagram.com/tejxv/')
+     }, 1000);
+            </script>
+        </div>
+    )
+}
+
+
+
+export default Redir;
