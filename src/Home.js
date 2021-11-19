3bedb4fce928a114ac04ada7dece790dec814f80
tejas <54097365+tejxv@users.noreply.github.com>
2021-11-20 00:25:06 +0530

    drive link

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ function Redir() {
         <div>
             <script>
             setTimeout(function() {
-    window.location.replace('https://rawcdn.githack.com/tejxv/tejxv.github.io/210179879053c9c4ac9a1efac30f6ab0a7b7e578/Resume%20-%20Tejas%20Upadhyay.pdf')
+    window.location.replace('https://drive.google.com/file/d/1CqApJJur5IDZtwrM4py3CA2Wbq4whJPQ/view?usp=sharing')
      }, 1000);
             </script>
         </div>
