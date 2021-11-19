081abe72cf8466dd6b75532eaa697c9411685d79
tejas <54097365+tejxv@users.noreply.github.com>
2021-11-19 23:08:29 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ function Redir() {
         <div>
             <script>
             setTimeout(function() {
-    window.location.replace('https://www.instagram.com/tejxv/')
+    window.location.replace('https://rawcdn.githack.com/tejxv/tejxv.github.io/210179879053c9c4ac9a1efac30f6ab0a7b7e578/Resume%20-%20Tejas%20Upadhyay.pdf')
      }, 1000);
             </script>
         </div>
