07f93d6f05ed6cec2df8b52bb8513ed91239a804
tejas <54097365+tejxv@users.noreply.github.com>
2022-01-23 00:55:51 +0530

    homepage fix

--- src/Resume.js
+++ src/Resume.js
@@ -5,7 +5,7 @@ function Redir() {
     return (
         <div className='pdfContainer'>
         this is outside
-        <iframe src="Resume - Tejas Upadhyay.pdf" width="100%" height="100%">
+        <iframe title="Resume" src="Resume - Tejas Upadhyay.pdf" width="100%" height="100%">
     </iframe>
         </div>
     )
